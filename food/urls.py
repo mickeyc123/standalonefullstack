6005ab8a71bad43305e912fb2d6bc09b7b8c0414
mickeyc123 <86480582+mickeyc123@users.noreply.github.com>
2024-03-27 17:12:14 +0000

    Delete food directory

--- food/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'food'  # Add this line to specify the app name
-
-urlpatterns = [
-    path('', views.index, name='index.html'),  # This is the root URL pattern
-    path('pizza/', views.pizza, name='pizza'),
-]
