7408caa6f6430a582bb9a669241ffc39df0e5d4e
mickeyc123 <michaelcampbell924@gmail.com>
2024-03-28 15:06:52 +0000

    update to pages and forloops pizza burger page

--- food/urls.py
+++ food/urls.py
@@ -6,4 +6,5 @@ app_name = 'food'  # Add this line to specify the app name
 urlpatterns = [
     path('', views.index, name='index'),  # This should point to your index view
     path('pizza/', views.pizza, name='pizza'),  # This is the root URL pattern
+    path('burger/', views.burger, name='burger'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza, Burger
 
 # Create your views here.
 def index(request):
-    ctx = {'name': 'good food'}  # Corrected context dictionary
+    ctx = {'name': 'welcome to our food'}  # Corrected context dictionary
     return render(request, 'food/index.html', ctx)  # Using render instead of HttpResponse
 
 def pizza(request):
-    return render(request, 'food/pizza.html')
+    pizzas = Pizza.objects.all()  
+    ctx = {'pizzas': pizzas}  
+    return render(request, 'food/pizza.html', ctx)  
 
+def burger(request):
+    burgers = Burger.objects.all()  # Corrected variable name 'burgers'
+    ctx = {'burgers': burgers}  # Corrected context dictionary key
+    return render(request, 'food/burger.html', ctx)  # Corrected template name
