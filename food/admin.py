c51b7bb868c8762399c0070fe6ed9600ab605118
mickeyc123 <michaelcampbell924@gmail.com>
2024-03-28 14:19:22 +0000

    i have added the burger to my admin page so user can upload burger

--- food/admin.py
+++ food/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Pizza
+from .models import Pizza,Burger
 
 @admin.register(Pizza)
 class PizzaAdmin(admin.ModelAdmin):
@@ -12,6 +12,20 @@ class PizzaAdmin(admin.ModelAdmin):
     def image_preview(self, obj):
         if obj.image:
             return mark_safe('<img src="{url}" width="100" />'.format(url=obj.image.url))
+        else:
+            return 'No Image'
+    
+    image_preview.short_description = 'Image Preview'
+    
+@admin.register(Burger)
+class BurgerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price_m', 'price_l', 'image_preview']
+    search_fields = ['name']
+    list_filter = ['price_m', 'price_l']
+
+    def image_preview(self, obj):
+        if obj.imageb:
+            return mark_safe('<img src="{url}" width="100" />'.format(url=obj.imageb.url))
         else:
             return 'No Image'
     
--- food/models.py
+++ food/models.py
@@ -9,3 +9,14 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Burger(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price_m = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True, verbose_name='Price (Medium)')
+    price_l = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True, verbose_name='Price (Large)')
+    imageb = models.ImageField(upload_to='pizza_images/', null=True, blank=True, verbose_name='Burger Image')
+
+    def __str__(self):
+        return self.name
