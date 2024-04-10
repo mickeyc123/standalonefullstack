49a047b3e0a27e2d7d2cdbba2898c10d269c5f26
mickeyc123 <michaelcampbell924@gmail.com>
2024-04-10 23:50:21 +0100

    finally!

--- makingfood/settings.py
+++ makingfood/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'makingfood.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
