3104086242b4b3586015fd1da0697a99aca91009
mickeyc123 <michaelcampbell924@gmail.com>
2024-04-11 00:12:24 +0100

    one more

--- makingfood/urls.py
+++ makingfood/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('', include('food.urls')),  # Include your app's URLs
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Add this line to serve media files during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
